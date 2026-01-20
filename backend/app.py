ba8b8b896757b04d7a79b4aa75d2604b6e329d16
Om Kapse <omkapse37@gmail.com>
2026-01-20 15:01:25 +0530

    added server config

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from routes.recommendation_routes import recommendation_bp
 from routes.dashboard_routes import dashboard_bp
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -14,4 +15,5 @@ def home():
     return {"status": "Backend is running"}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
